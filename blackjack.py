ef6fa915732a4584a70d49e17888475b3b5a5218
Manny A <emanuelantablin@protonmail.com>
2020-04-06 11:33:32 -0600

    Refactored hitCard method to allow easier use

--- blackjack.py
+++ blackjack.py
@@ -1,19 +1,35 @@
 import random
 
 
-# print("Hello World!")
+class gameLogic:
+    # TODO: Move game functionality here
 
+    def hitCard(self):
 
-# def hitCard():
-#     return(str(random.randrange(2, 11)))
+        if sum(player.playerCards) > 21:
+            print("Dealer wins! You've gone over 21 points. Total: " + str(sum(player.playerCards)))
+        if sum(player.playerCards) < 21:
+            print("""Would you like to "hit" or "stay"?""")
+            # PSEDUOCODE: 
+            # if console.readLine() == "hit":
+            #   player.playerCards.append(self.hitCard())
+            # if console.readLine() == "stay":
+            #   if dealer.points <= 16:
+            #       dealer.hitCard()    
+            #   
+
+        
 
 class player:
-    playerPoints = 0
-    playerCards = []
+    playerCards = [random.randrange(2,11), random.randrange(2,11)] #Holds player cards, points can be figured out on the spot
 
 
     def hitCard(self):
-        return(random.randrange(2, 11))
+        # return(random.randrange(2, 11))
+        self.playerCards.append(random.randrange(2,11))
+        print(self.playerCards)
+        
+        
 
     def gamePrinter(self):
         player.playerCards.append(self.hitCard()) # Give player initial card 0
@@ -24,29 +40,18 @@ class player:
         print(player.playerCards)
 
 class dealer:
-    dealerPoints = 0
-    delearCards = []
+    delearCards = [random.randrange(2,11), random.randrange(2,11)] #Holds dealer cards, points can be figured out on the spot
 
+    # def hitCard(self):
 
+    # Dealer can hit on <= 16
+    # if dealer.points > 21:
+    #   player Wins
 
+# Test game
 p = player()
+p.hitCard()
 
-p.gamePrinter()
-
-
-
-
-
-
-
-
-
-
-
-# class GameLogic:
-#     playerPoints, dealerPoints = 0 # Keep Score of points
-
-#     # Trackers Below
-#     playerHitCounter = 0 # Simple statistic to keep track of
-
-#     # Every call will provide new card values
+# NOTES
+# Dealer and player seem to need their own hitCard function, seeing as the dealer will have certain constraints that the player won't
+# i.e: dealer won't be hitting anything if they have more than 16 points whereas player can wing it
