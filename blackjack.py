b88b75d6c2c11805503bcc99f27728bee514aca5
Manny A <emanuelantablin@protonmail.com>
2020-04-06 12:16:31 -0600

    Refactored gameLogic

--- blackjack.py
+++ blackjack.py
@@ -4,46 +4,55 @@ import random
 class gameLogic:
     # TODO: Move game functionality here
 
-    def hitCard(self):
+    if sum(player.playerCards) > 21:
+        print("Dealer wins! You've gone over 21 points. Total: " + str(sum(player.playerCards)))
+    if sum(player.playerCards) < 21:
+        print("""Would you like to "hit" or "stay"?""")
+        # PSEDUOCODE: 
+        # if console.readLine() == "hit":
+        #   player.playerCards.append(self.hitCard())
+        # if console.readLine() == "stay":
+        #   if dealer.points <= 16:
+        #       dealer.hitCard()    
+        #   
 
-        if sum(player.playerCards) > 21:
-            print("Dealer wins! You've gone over 21 points. Total: " + str(sum(player.playerCards)))
-        if sum(player.playerCards) < 21:
-            print("""Would you like to "hit" or "stay"?""")
-            # PSEDUOCODE: 
-            # if console.readLine() == "hit":
-            #   player.playerCards.append(self.hitCard())
-            # if console.readLine() == "stay":
-            #   if dealer.points <= 16:
-            #       dealer.hitCard()    
-            #   
+    def gamePrinter(self):
+        print("Welcome to Manny's Blackjack program!")
+        print("You get a " + str(player.playerCards[0]) + " and a " + str(player.playerCards[1]))
+        print("Your total is " + str(sum(player.playerCards)))
+        print("The dealer has a {} showing, and a hidden card", )
+        print(player.playerCards)
 
         
 
 class player:
-    playerCards = [random.randrange(2,11), random.randrange(2,11)] #Holds player cards, points can be figured out on the spot
+    # Player attributes
+    playerCards = [random.randrange(2,11), random.randrange(2,11)] #Holds player cards, points can be figured out on the spot, initialized to provide 2 first cards
 
-
+    # Player functionality
     def hitCard(self):
-        # return(random.randrange(2, 11))
         self.playerCards.append(random.randrange(2,11))
-        print(self.playerCards)
-        
-        
+        if sum(playerCards) == 21:
+            print("Blackjack! You win!")
+        if sum(playerCards) > 21:
+            print("House wins! Your score: " + str(sum(playerCards)))
 
-    def gamePrinter(self):
-        player.playerCards.append(self.hitCard()) # Give player initial card 0
-        player.playerCards.append(self.hitCard()) # Give player initial card 1
-        print("Welcome to Manny's Blackjack program!")
-        print("You get a " + str(player.playerCards[0]) + " and a " + str(player.playerCards[1]))
-        print("Your total is " + str(sum(player.playerCards)))
-        print(player.playerCards)
+        print(self.playerCards)
+
 
 class dealer:
-    delearCards = [random.randrange(2,11), random.randrange(2,11)] #Holds dealer cards, points can be figured out on the spot
+    # Dealer attributes
+    delearCards = [random.randrange(2,11), random.randrange(2,11)] #Holds dealer cards, points can be figured out on the spot, initialized to provide 2 first cards
 
-    # def hitCard(self):
+    # Dealer functionality
+    def hitCard(self):
+        self.dealerCards.append(random.randrange(2,11))
+        if sum(dealerCards) == 21:
+            print("Blackjack! You win!")
+        if sum(dealerCards) > 21:
+            print("Player wins! House scored: " + str(sum(dealerCards)))
 
+        print(self.dealerCards)
     # Dealer can hit on <= 16
     # if dealer.points > 21:
     #   player Wins
