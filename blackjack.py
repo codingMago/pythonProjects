2638d49abb169334ae371d599f33f34805c67aa8
Manny A <emanuelantablin@protonmail.com>
2020-04-10 11:46:47 -0600

    Moved logic from player to gamelogic

--- blackjack.py
+++ blackjack.py
@@ -1,44 +1,23 @@
 import random
 
 
-class gameLogic:
-    # TODO: Move game functionality here
-
-    if sum(player.playerCards) > 21:
-        print("Dealer wins! You've gone over 21 points. Total: " + str(sum(player.playerCards)))
-    if sum(player.playerCards) < 21:
-        print("""Would you like to "hit" or "stay"?""")
-        # PSEDUOCODE: 
-        # if console.readLine() == "hit":
-        #   player.playerCards.append(self.hitCard())
-        # if console.readLine() == "stay":
-        #   if dealer.points <= 16:
-        #       dealer.hitCard()    
-        #   
-
-    def gamePrinter(self):
-        print("Welcome to Manny's Blackjack program!")
-        print("You get a " + str(player.playerCards[0]) + " and a " + str(player.playerCards[1]))
-        print("Your total is " + str(sum(player.playerCards)))
-        print("The dealer has a {} showing, and a hidden card", )
-        print(player.playerCards)
-
-        
 
 class player:
     # Player attributes
     playerCards = [random.randrange(2,11), random.randrange(2,11)] #Holds player cards, points can be figured out on the spot, initialized to provide 2 first cards
 
     # Player functionality
+    def getCards(self):
+        return self.playerCards
+        # print(self.playerCards)
     def hitCard(self):
         self.playerCards.append(random.randrange(2,11))
-        if sum(playerCards) == 21:
+        if sum(self.playerCards) == 21:
             print("Blackjack! You win!")
-        if sum(playerCards) > 21:
+        if sum(self.playerCards) > 21:
             print("House wins! Your score: " + str(sum(playerCards)))
 
         print(self.playerCards)
-
 
 class dealer:
     # Dealer attributes
@@ -57,9 +36,38 @@ class dealer:
     # if dealer.points > 21:
     #   player Wins
 
+class gameLogic:
+    # TODO: Move game functionality here
+
+    blablaguy = player()
+    dealer0 = dealer()
+
+    def gamePrinter(self):
+        print("Welcome to Manny's Blackjack program!")
+        print("You get a " + str(self.blablaguy.playerCards[0]) + " and a " + str(self.blablaguy.playerCards[1]))
+        print("Your total is " + str(sum(self.blablaguy.playerCards)))
+        print("The dealer has a " + str(self.dealer0.delearCards[0]) + " showing, and a hidden card")
+        print(self.blablaguy.playerCards)
+
+    # if sum(self.blablaguy.playerCards) > 21:
+    #     print("Dealer wins! You've gone over 21 points. Total: " + str(sum(self.blablaguy.playerCards)))
+    # if sum(self.blablaguy.playerCards) < 21:
+    #     print("""Would you like to "hit" or "stay"?""")
+        # PSEDUOCODE: 
+        # if console.readLine() == "hit":
+        #   player.playerCards.append(self.hitCard())
+        # if console.readLine() == "stay":
+        #   if dealer.points <= 16:
+        #       dealer.hitCard()    
+        #   
+
+        
+
 # Test game
-p = player()
-p.hitCard()
+# p = player()
+# p.getCards()
+g = gameLogic()
+g.gamePrinter()
 
 # NOTES
 # Dealer and player seem to need their own hitCard function, seeing as the dealer will have certain constraints that the player won't
