dc05a1ac2f4c87809cdfe82a19df34a120d5c117
Manny A <emanuelantablin@protonmail.com>
2020-04-10 12:14:22 -0600

    Adding user input functionality + bug squashing

--- blackjack.py
+++ blackjack.py
@@ -12,12 +12,12 @@ class player:
         # print(self.playerCards)
     def hitCard(self):
         self.playerCards.append(random.randrange(2,11))
-        if sum(self.playerCards) == 21:
-            print("Blackjack! You win!")
-        if sum(self.playerCards) > 21:
-            print("House wins! Your score: " + str(sum(playerCards)))
+        # if sum(self.playerCards) == 21:
+        #     print("Blackjack! You win!")
+        # if sum(self.playerCards) > 21:
+        #     print("House wins! Your score: " + str(sum(playerCards)))
 
-        print(self.playerCards)
+        # print(self.playerCards)
 
 class dealer:
     # Dealer attributes
@@ -26,12 +26,10 @@ class dealer:
     # Dealer functionality
     def hitCard(self):
         self.dealerCards.append(random.randrange(2,11))
-        if sum(dealerCards) == 21:
-            print("Blackjack! You win!")
-        if sum(dealerCards) > 21:
-            print("Player wins! House scored: " + str(sum(dealerCards)))
-
-        print(self.dealerCards)
+        # if sum(dealerCards) == 21:
+        #     print("Blackjack! You win!")
+        # if sum(dealerCards) > 21:
+        #     print("Player wins! House scored: " + str(sum(dealerCards)))
     # Dealer can hit on <= 16
     # if dealer.points > 21:
     #   player Wins
@@ -40,14 +38,28 @@ class gameLogic:
     # TODO: Move game functionality here
 
     blablaguy = player()
+    pTotal = sum(blablaguy.playerCards)
     dealer0 = dealer()
+    # dTotal = sum(dealer0.dealerCards)
 
     def gamePrinter(self):
         print("Welcome to Manny's Blackjack program!")
-        print("You get a " + str(self.blablaguy.playerCards[0]) + " and a " + str(self.blablaguy.playerCards[1]))
-        print("Your total is " + str(sum(self.blablaguy.playerCards)))
+        print("You get an " + str(self.blablaguy.playerCards[0]) + " and a " 
+        + str(self.blablaguy.playerCards[1]) + ". Total: " + str(self.pTotal))
         print("The dealer has a " + str(self.dealer0.delearCards[0]) + " showing, and a hidden card")
-        print(self.blablaguy.playerCards)
+
+        while self.pTotal < 21:
+            userChoice = input("""Would you like to "H"it or "S"tay?""")
+
+            if userChoice == "H" or userChoice == "h":
+                self.blablaguy.hitCard()
+
+            elif userChoice == "S" or userChoice == "s":
+                print("Your current total: " + str(self.pTotal))
+
+        # Pseudocode for implementing reading user input
+        # if
+        # print(self.blablaguy.playerCards)
 
     # if sum(self.blablaguy.playerCards) > 21:
     #     print("Dealer wins! You've gone over 21 points. Total: " + str(sum(self.blablaguy.playerCards)))
