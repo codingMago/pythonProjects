ff8b83ef617d9ea728894c1263ad402d2ef7a8d4
secimax <secimax@pop-os.localdomain>
2020-12-23 12:37:32 -0800

    Added initial b2d and simple calculator

---
+++ projects/numbers/binarytodecimal.py
@@ -0,0 +1,24 @@
+
+def binarytodecimal():
+    return True
+
+def decimaltobinary(number: int):
+
+    output = []
+    isEven = True
+    if number % 2 == 1:
+        isEven = False
+    
+    if isEven == False:
+        output.append(1)
+
+
+
+
+""" Notes
+Binary
+32768 16384 8192 4096 2048 1024 612 256 128 64 32 16 8 4 2 1
+
+
+"""
+
---
+++ projects/numbers/calculator.py
@@ -0,0 +1,12 @@
+def addition(a: int, b: int):
+    return a + b
+
+def substraction(a: int, b: int):
+    return a - b
+
+def multiplication(a: int, b: int):
+    return a * b
+
+def division(a: int, b: int):
+    return a / b
+
