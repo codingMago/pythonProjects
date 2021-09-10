81c4c7ed639612c27027348468f01dcc5b5313ba
eantablin <eantablin@protonmail.com>
2021-09-10 10:48:37 -0700

    Changes to iOS game

--- projects/games/iOS/TextGame/textgame.py
+++ projects/games/iOS/TextGame/textgame.py
@@ -6,12 +6,13 @@ from random import randint # Used for eightBall
 
 # Shorthand functions 
 cls = lambda: console.clear() # Clear console
-peaSound = lambda: sound.play_effect('8ve:8ve-beep-timber') # Peasant's signature sound
-nobSound = lambda: sound.play_effect('game:Woosh_1') # Nobleman's signature sound
-roySound = lambda: sound.play_effect('game:Ding_3') # Royalty's signature sound
+peasantSound = lambda: sound.play_effect('8ve:8ve-beep-timber') # Peasant's signature sound
+nobleSound = lambda: sound.play_effect('game:Woosh_1') # Nobleman's signature sound
+royalSound = lambda: sound.play_effect('game:Ding_3') # Royalty's signature sound
+easyaddition = lambda: print("Hello World")
 
 class Player():
-	level = 0 # Track progress
+	level = 0 # Track of progress
 	health = 0 # Track HP
 	currentHP = health
 	stamina = 0 # Track energy
@@ -25,7 +26,7 @@ class Player():
 	def classChoice(self, choice): # Initial class choice
 
 		if choice == 1: # Peasant
-			peaSound()
+			peasantSound()
 			self.level = 1
 			self.health = 60
 			self.currentHP = self.health
@@ -36,7 +37,7 @@ class Player():
 			self.className = "Peasant"
 			# Total = 140
 		elif choice == 2: # Nobleman
-			nobSound()
+			nobleSound()
 			self.level = 1
 			self.health = 100
 			self.currentHP = self.health
@@ -47,7 +48,7 @@ class Player():
 			self.className = "Nobleman"
 			# Total = 155
 		elif choice == 3: # Royalty
-			roySound()
+			royalSound()
 			self.level = 1
 			self.health = 100
 			self.currentHP = self.health
@@ -62,7 +63,7 @@ class Player():
 		
 	# Use on user XP gain
 	# Perhaps later make it so bosses can level up too
-	def islevelUP(self, experience):
+	def checklevelUP(self, experience):
 
 		totalXP = self.getXP() + experience # Keep track of current total experience
 
@@ -100,36 +101,50 @@ class Player():
 		return self.level
 	def setLVL(self, value):
 		self.level = value
+	def changeLVL(self, value):
+		self.level += value
 	
 	def getHP(self):
 		return self.health
 	def setHP(self, value):
 		self.health = value
+	def changeHP(self, value):
+		self.health += value
 
 	def getCurrentHP(self):
 		return self.currentHP
 	def setCurrentHP(self, value):
 		self.currentHP = value
+	def changeCurrentHP(self, value):
+		self.currentHP += value
 	
 	def getStam(self):
 		return self.stamina
 	def setStam(self, value):
 		self.stamina = value
+	def changeStam(self, value):
+		self.stamina += value
 	
 	def getMana(self):
 		return self.mana
 	def setMana(self, value):
 		self.mana = value
+	def changeMana(self, value):
+		self.mana += value
 		
 	def getXP(self):
 		return self.xp
 	def setXP(self, value):
 		self.xp = value
+	def changeXP(self, value):
+		self.xp += value
 		
 	def getGP(self):
 		return self.gp
 	def setGP(self, value):
 		self.gp = value
+	def changeGP(self, value):
+		self.gp += value
 	
 class Enemy():
 	HP = 0 # Health
@@ -147,29 +162,47 @@ class Enemy():
 	def randomEnemy(self, number): # Takes a number between 1-100, set enemy depending on user luck
 
 		if number <= 10: # Strong mob, 10% chance
-			self.__init__(80, 10, 50, 20)
+			self.HP = 80
+			self.MP = 10
+			self.XP = 50
+			self.DMG = 20
 			self.Type = "Troll"
 		if number <= 50 and number > 10: # Trash mob, 40% chance
-			self.__init__(20, 0, 10, 5)
+			self.HP = 20
+			self.MP = 0
+			self.XP = 10
+			self.DMG = 5
 			self.Type = "Goblin"
 		if number <= 80 and number > 50: # Tier 2 trash mob, 30% chance
-			self.__init__(30, 0, 20, 10)
+			self.HP = 30
+			self.MP = 0
+			self.XP = 20
+			self.DMG = 10
 			self.Type = "Orc"
 		if number <= 85 and number > 80: # Free xp mob, 5% chance
-			self.__init__(40, 0, 30, 0)
+			self.HP = 40
+			self.MP = 0
+			self.XP = 30
+			self.DMG = 0
 			self.Type = "Bear cub"
 		if number <= 90 and number > 85: # Boss mob, 5% chance
-			self.__init__(80, 40, 60, 30)
+			self.HP = 80
+			self.MP = 40
+			self.XP = 60
+			self.DMG = 30
 			self.Type = "Wizard"
 		if number <= 100 and number > 90: # Medium mob, 10% chance
-			self.__init__(100, 0, 40, 15)
+			self.HP = 100
+			self.MP = 0
+			self.XP = 40
+			self.DMG = 15
 			self.Type = "Ogre"
 
 
 	
 class RPGame():
 	
-	isAlive = True
+	isAlive = True # Game still running?
 	
 	def startGame(self, player):
 		
@@ -184,6 +217,7 @@ class RPGame():
 			self.adventure(player)
 			# implement timer, 1-2 seconds
 	
+	# Allows user to pick a class
 	def characterChoice(self, player):
 		
 		while self.isAlive == True: # While user hasn't decided to exit
@@ -192,10 +226,10 @@ class RPGame():
 			
 			if userChoice == 1: # User might pick to be a peasant
 				cls()
-				peaSound()
+				peasantSound()
 
 				print("Due to years of backbreaking work, Caldria's peasants are known to have a high tolerance for even the most grueling of tasks.\n\n")
-				classChoice = int(input("Is this your class?\n1. Yes, I'm a peasant\n0. No, let me check others"))
+				classChoice = int(input("Is this your class?\n1. Yes, I'm a Peasant\n0. No, let me check others"))
 				
 				if classChoice == 1:
 					player.classChoice(userChoice)
@@ -203,7 +237,7 @@ class RPGame():
 
 			elif userChoice == 2: # User might choose a nobleman
 				cls()
-				nobSound()
+				nobleSound()
 
 				print("Born to a strong house with servants aplenty, the Caldrian noblemen are considered chivalrous and known to be healthy.\n\n")
 				classChoice = int(input("Is this your class?\n1. Yes, I'm a Nobleman\n0. No, let me check others"))
@@ -214,7 +248,7 @@ class RPGame():
 			
 			elif userChoice == 3: # Perhaps user went for royalty
 				cls()
-				roySound()
+				royalSound()
 
 				print("Caldria's royalty are renown for their short temper, it's said that the magical books they looted from surrounding nations have essentially changed them.\n\n")
 				classChoice = int(input("Is this your class?\n1. Yes, I'm Royalty\n0. No, let me check others"))
@@ -238,19 +272,42 @@ class RPGame():
 			cls()
 			
 			if userChoice == 1: # Explore
+				encounter = randint(1, 7)
+
+				if encounter == 1:
+					return
+					# Encounter stranger
+				elif encounter == 2:
+					return 
+					# Traveling merchant
+				elif encounter == 3:
+					return 
+					# Find treasure
+				elif encounter == 4: 
+					return 
+					# Dungeon time
+				elif encounter == 5:
+					return 
+					# Found a Town
+				elif encounter > 5: # ATM should have 2 rolls in 7, 28.57% chance
+					self.combat(player)
+
 				# In order of priority
 				# Add chance for combat, traveling merchant, find treasure, dungeon, town
-				# encounter = randint(1, 100) # Decides what encounter to launch
-				# if encounter <= 33:
-				# 	self.combat(player)
-				# elif encounter > 33 and encounter < 
-				self.combat(player)
+
 
 			elif userChoice == 2: # Inventory
 				print('No items collected')
 			elif userChoice == 3: # Player stats
-				if player.getHP() >= 50:
-					print("I'm okay")
+				playerHP = player.getHP()
+				if playerHP >= 75:
+					print("I'll be fine")
+				elif playerHP >= 50 and playerHP < 75:
+					print("Some cuts and bruises, I'm otherwise okay")
+				elif playerHP >= 25 and playerHP < 50:
+					print("I'll need healing soon")
+				elif playerHP >= 1 and playerHP < 25:
+					print("Need healing, I might not make it")
 				else:
 					print("I could use some healing")
 			elif userChoice == 0: # Exit game
@@ -259,16 +316,16 @@ class RPGame():
 	def combat(self, player):
 
 		combatHP = player.getCurrentHP() # Keep track of health
-		combatMP = player.getMana()
-		combatEnergy = player.getStam()
+		combatMP = player.getMana() # Track mana
+		combatEnergy = player.getStam() # Track stamina
 		randNumber = randint(1, 100) # Create random variable to assign enemy encounter
-		opponent = Enemy() # Instantiate enemy variable
+		opponent = Enemy() # Instantiate enemy object
 		opponent.randomEnemy(randNumber) # Assign random enemy
-		
+		enemyHP = opponent.HP
 
-		while combatHP > 0 or self.isAlive == True: # While player is still in the game
+		while combatHP > 0 or self.isAlive == True or enemyHP > 0: # While player is still in the game
 
-			combatEnergy += 5 # Player regenerates HP
+			combatEnergy += 5 # Player regenerates stamina
 
 			cls()
 			self.displayCombatStats(player, combatMP)
@@ -291,13 +348,17 @@ class RPGame():
 
 
 	def displayCombatStats(self, player, mana): # Keep track of current combat stats
-		print(f'{player.className}\nHP: {player.getCurrentHP()}/{player.getHP} | MP: {mana}/{player.getMana()}\n')
+		print(f'{player.className}\nHP: {player.getCurrentHP()}/{player.getHP()} | MP: {mana}/{player.getMana()}\n')
 	
 	def displayStats(self, player): # For most menus
-		print(f'{player.className}\nHP: {player.getCurrentHP()}/{player.getHP} | MP: {player.getMana()}\n')
+		print(f'{player.className}\nHP: {player.getCurrentHP()}/{player.getHP()} | MP: {player.getMana()}\n')
 	
 	def displayFullStats(self, player): # For when user wants/needs to see their total stats
-		print(f'{player.className}\nHP: {player.getCurrentHP()}/{player.getHP} | MP: {player.getMana()}\nGP: {player.getGP()} | XP: {player.getXP()}\n')
+		print(f'{player.className}\nHP: {player.getCurrentHP()}/{player.getHP()} | MP: {player.getMana()}\nGP: {player.getGP()} | XP: {player.getXP()}\n')
+		
+	def displayInventory(self, player):
+		return
+		# print(f'player.')
 
 
 
